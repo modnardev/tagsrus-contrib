71276e2eb4dac46d06a10dbd828f7209905c1df2
Trevor Starick <trevor.starick@gmail.com>
2022-11-20 01:02:15 -0500

    Update sampler.sh

--- reddit_sampler/sampler.sh
+++ reddit_sampler/sampler.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-HOST="http://127.0.0.1:8080"
-AFTER=""
+# HOST="http://192.168.1.100:8888" # sfw
+HOST="http://192.168.1.100:8889" #nsfw
+# HOST="http://localhost:8090" #mixed
+MAX=500
+
+PAGES=( \
+# "https://www.reddit.com/r/all" \
+)
+
+# hour, day, week, month, year, all
+T="month"
 
 get () {
-    out=$(curl -s -A 'User-Agent: tagsrus-sampler/1.0.0' \
-    "https://www.reddit.com/r/all/top.json?t=all&after=$AFTER" \
-    | jq -r '.data.children[]|.data.name')
+    LINK="$PAGE/top.json?t=$T&after=$AFTER"
+    echo "-- $LINK"
+    out=$(curl -s -A 'User-Agent: tagsrus-sampler/1.0.0' $LINK \
+    | jq -r '.data.children[]|.data.name+";"+.data.permalink')
 
     for i in $out; do
-        curl -s "$HOST/api/import?link=https://reddit.com/${i:3}" > /dev/null
-        echo ${i:3}
-        AFTER=$i
+        j=(${i//;/ })
+        echo ${j[0]:3} ${j[1]}
+        curl -s "$HOST/api/import?link=https://www.reddit.com${j[1]}" > /dev/null
+        AFTER=${j[0]}
     done
 }
 
-for i in $(seq 1 1 20); do
-    get
+for PAGE in ${PAGES[@]}; do
+    AFTER=""
+    for i in $(seq 1 25 $(($MAX-1))); do
+        get $PAGE
+    done
 done
